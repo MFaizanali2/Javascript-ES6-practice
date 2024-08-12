8a2401bd7dd7e6967f1e39b0a4946edab58df0eb
Faizan Ali <154216258+MFaizanali2@users.noreply.github.com>
2024-08-12 11:17:27 +0500

    Update app.js

---
+++ Spread-&-Rest-Operator/app.js
@@ -0,0 +1,17 @@
+    // simple function
+// function sum(num1, num2){
+//     console.log(num1 + num2 )
+// }
+// sum(20,78)
+
+
+    // Rest Operator
+// function sum(name, ...arg){
+//     let sum = 0 ;
+//     for(let i in arg){
+//         sum += arg[i]
+//     }
+//     console.log(name);
+//     console.log(arg)
+// }
+// sum("hello", "nice", "billi", 75, 90, 54, 845, 65,);
