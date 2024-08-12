0948eb29dfac55f1d31cf99c3814e1b135217f0e
Faizan Ali <154216258+MFaizanali2@users.noreply.github.com>
2024-08-12 17:30:02 +0500

    Update app.js

--- Sheriyans-video-ES6/app.js
+++ Sheriyans-video-ES6/app.js
@@ -9,4 +9,34 @@
 // const num = 89;
 // console.log(num)
 
+// let or const ma kuch similarities ha dono window ma add hota ha or dono braces scope hai
 
+
+    // ES6 Arrow Function
+// arrow function 3 types ka hai
+//01) Basic fat arrow function?
+// const upload = () =>{
+
+// }
+// upload function name ha
+
+// 02) Fat arrow function with one parameter?
+// const upload = (a) =>{
+//     console.log(a)
+// }
+// upload(5)
+
+// 02) Fat arrow function with impliciat return
+// const g = () => 117;
+// console.log(g());
+
+
+
+    // ES6 template literals backtick
+// let sum = 5+9;
+// let num = 5*9;
+// console.log(`The answer of sum is ${sum} and num is ${num}`);
+
+// double quotaion ka andar ham variable ko nahi likh shakta ha backtick ma dollar sign ka sath 
+// use kar shakta ha, double quotaion ka andar sum or num sa calculation nahi hoti ha wo sum or num ko
+// string store kara ga.
