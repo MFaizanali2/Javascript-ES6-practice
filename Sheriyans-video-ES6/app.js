5cf809ff2e0f63c993123dbb27c3d5d89ac8b532
Faizan Ali <154216258+MFaizanali2@users.noreply.github.com>
2024-08-12 17:11:45 +0500

    Update app.js

--- Sheriyans-video-ES6/app.js
+++ Sheriyans-video-ES6/app.js
@@ -1,9 +1,12 @@
-    // ES6 Let
+    // ES6 Let let ki value change kar shakta ha
 // let num = 20;
 // num = 35;
 // num = 67;
 // console.log(num)
 
 
-const num = 89;
-console.log()
+    // ES6 Const const ki value ham change nahi kar shakta ha 
+// const num = 89;
+// console.log(num)
+
+
